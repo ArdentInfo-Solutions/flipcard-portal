712dae51e1177af74c5b099f1fa3ba6ab5602f1b
PardeepSingh2002 <159152070+PardeepSingh2002@users.noreply.github.com>
2025-03-24 11:06:49 +0530

    Removed exposed secrets

--- src/app/web/page.tsx
+++ src/app/web/page.tsx
@@ -2,7 +2,7 @@
 
 import TabBar from "../../components/common/TabBar"
 import { WebItemFeed } from "../../components/page-components/web-feed/WebItemFeed"
-import OtherApp from "../../components/appcomponents/OtherApp"
+
 
 
 export default function WebPage() {
@@ -11,7 +11,7 @@ export default function WebPage() {
       <div className="hidden md:block w-full">
         <TabBar />
       </div>
-        <OtherApp/>
+       
         <WebItemFeed />
       <div className="block md:hidden">
         <TabBar />
